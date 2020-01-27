64b0add7a273ea9c0027ddf9c03577760b1a0456
Madaspe <Madaspeee@yandex.ru>
2020-01-27 05:26:22 -0800

    Fix

--- startProgramm.py
+++ startProgramm.py
@@ -1,13 +1,14 @@
 from PyQt5 import Qt
 from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
 from sys import argv, exit
-from mainPage import Main_page
-from loginPage import Login_page
-from inc import Pass_inc_page
-from addUser import AddUserI
-from removeUser import RemoveUserI
-from camera import Camera
-from sql import select_user_sql, add_user_sql, remove_user_sql
+from interface.mainPage import Main_page
+from interface.loginPage import Login_page
+from interface.inc import Pass_inc_page
+from interface.addUser import AddUserI
+from interface.removeUser import RemoveUserI
+from interface.camera import Camera
+from interface.sql import select_user_sql, add_user_sql, remove_user_sql
+from video_stream import process_video_stream
 
 
 class IpCamera(Camera, QMainWindow):
@@ -62,10 +63,14 @@ class Main_pages(Main_page, QMainWindow):
         self.pushButton_4.clicked.connect(self.add_user)
         self.pushButton_5.clicked.connect(self.remove_user)
         self.pushButton.clicked.connect(self.ip_camera)
+        self.pushButton_7.clicked.connect(self.stream)
         self.addUser = AddUserPage(self)
         self.removeUser = RemoveUserPage(self)
         self.pushButton_6.clicked.connect(self.exit)
         self.ipCamera = IpCamera(self)
+
+    def stream(self):
+        process_video_stream()
 
     def ip_camera(self):
         self.ipCamera.show()
@@ -99,17 +104,21 @@ class Login_page(Login_page, QMainWindow):
         self.pushButton.clicked.connect(self.login)
 
     def login(self):
-        temp = select_user_sql(self.lineEdit.text())
+        # temp = select_user_sql(self.lineEdit.text())
         if self.lineEdit.text() == 'admin' and self.lineEdit_2.text() == 'admin':
             self.main_page = Main_pages(self)
             self.main_page.show()
             self.hide()
         else:
-            pass
+            self.passInc.show()
 
 
-if __name__ == '__main__':
+def main():
     app = QApplication(argv)
     ex = Login_page()
     ex.show()
     exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
